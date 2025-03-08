cce452ff5f307b8415d2457b9ec89411d419a849
midhunpallampetty <midhunpallampetty@gmai.com>
2025-03-08 12:39:56 +0530

    test commit after ui animation

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -1,6 +1,10 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 const   client=new ApolloClient({
+<<<<<<< HEAD
     uri:'https://skybeats.site/graphql',
+=======
+    uri:'http://localhost:3300/graphql',
+>>>>>>> 97fc021 (test commit after ui animation)
     cache:new InMemoryCache(),
 });
 export default client;
